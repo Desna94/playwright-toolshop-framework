d6c4753549efbc3c393ed0206835f80a55b3c421
Oleh D <desna.oleh@gmail.com>
2026-09-03 18:43:09 +0200

    test: add invalid login scenario

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -18,3 +18,22 @@ test("user can log in with valid credentials", async ({ page }) => {
     "My account",
   );
 });
+
+test("user cannot log in with invalid credentials", async ({ page }) => {
+  //Arrange
+  const email = "invalid@example.com";
+  const password = "wrongpassword";
+
+  await page.goto("/auth/login");
+
+  //Act
+  await page.locator('[data-test="email"]').fill(email);
+  await page.locator('[data-test="password"]').fill(password);
+  await page.locator('[data-test="login-submit"]').click();
+
+  //Assert
+  await expect(page).toHaveURL("/auth/login");
+  await expect(page.locator('[data-test="login-error"]')).toHaveText(
+    "Invalid email or password",
+  );
+});
