9dd4c83a297dd2bb63d843e7850a81b6a578b276
Oleh D <desna.oleh@gmail.com>
2026-09-13 18:23:44 +0200

    test: add invalid resource validation

--- tests/api/validation.api.spec.ts
+++ tests/api/validation.api.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from "@playwright/test";
 import type { ApiErrorResponse } from "../../src/api/models/error.model";
 import { AuthApiClient } from "../../src/api/clients/auth.client";
+import { ProductsApiClient } from "../../src/api/clients/products.client";
 
 test("unauthorized request validation", async ({ request }) => {
   const authApi = new AuthApiClient(request);
@@ -12,4 +13,18 @@ test("unauthorized request validation", async ({ request }) => {
   const body = (await response.json()) as ApiErrorResponse;
 
   expect(body.message).toBe("Unauthorized");
+});
+
+test("invalid resource validation", async ({ request }) => {
+  const nonExistingProductId = "non-existing-product-id";
+
+  const productsApi = new ProductsApiClient(request);
+  const response = await productsApi.getProduct(nonExistingProductId);
+
+  expect(response.status()).toBe(404);
+  expect(response.headers()["content-type"]).toContain("application/json");
+
+  const body = (await response.json()) as ApiErrorResponse;
+
+  expect(body.message).toBe("Requested item not found");
 });
